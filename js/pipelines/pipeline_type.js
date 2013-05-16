88acd09e91570d98c356fa04d83e2270057c8593
juanantonioruz <juanantonioruz@gmail.com>
2013-05-16 21:29:28 +0200

    pipelines synchronous composition done ok

--- js/pipelines/pipeline_type.js
+++ js/pipelines/pipeline_type.js
@@ -4,15 +4,23 @@ define(["js/fiber.min.js","js/pipelines/state_step_type.js","js/async.js"],
 
             var Pipeline=Fiber.extend(function(){
                 return  {
-                    init: function(name) {
+                    init: function(name,on_success, on_error) {
                         this.ns=name;
                         this.future_state_steps=[];
+                        this.on_success=on_success;
+                        this.on_error=on_error;
                         return this;
                     },
                     addTransformation:function(ns, transformation_fn){
                         this.future_state_steps.push(new StateStep(ns, transformation_fn));
                         return this;
                     },
+                    addPipe:function(pipe){
+                        this.future_state_steps.push(pipe);
+                        return this;
+                    },
+                    
+
                     getStateStep:function(ns){
                         for(var i=0 ; i<this.future_state_steps.length; i++){
                             var internalStateStep= this.future_state_steps[i];
@@ -34,7 +42,17 @@ define(["js/fiber.min.js","js/pipelines/state_step_type.js","js/async.js"],
                         $('#status').fadeIn();
                         this.before_data_state=$.extend(true, {}, data_state);
                     },
-                    transform:function(data_state, on_success, on_error){
+                    set_on_success:function(fn){
+                        this.on_success=fn;
+                        return this;
+                    },
+                    set_on_error:function(fn){
+                        this.on_error=fn;
+                        return this;
+                    },
+                   
+
+                    apply_transformations:function(data_state){
                         var that=this;
                         // this function composition use the method transform of each statestep in the context of each step (bind) ... the funcions for composing have to be rearranged in reverse order
                         var composition=async.compose.apply(null, this.future_state_steps.reverse().map(function(o){return o.transform.bind(o);}));
@@ -45,20 +63,40 @@ define(["js/fiber.min.js","js/pipelines/state_step_type.js","js/async.js"],
                         composition($.extend(true, {}, data_state),function(err, res){
 
                             if(!err){
+
                                 that.on_end(res);
-                                on_success(res, that);
+                                that.on_success(res, that);
+
                             }else{
-                                on_error(err, that);
+                                that.on_error(err, that);
+
                             }
 
                         });
 
-
-
-
-
                         
-                    }
+                    },
+                    // same api as state step
+                      transform:function(data_state, callback){
+                          var that=this;
+                           data_state.user_history.push("this.ns try to fired pipeline: "+this.ns);
+                          alert("try to transform "+this.ns);
+                          //this.apply_transformations(data_state);
+
+
+                           var actual=this.on_success;
+
+                          var extended=function(res, that){
+                              actual(res, that);
+                              callback(null, res);
+                              this.on_success=actual;
+                           };
+                          this.set_on_success(extended);
+                           this.apply_transformations(data_state);
+                          
+
+                       }
+
                 };
             });
 
--- js/pipelines/start.js
+++ js/pipelines/start.js
@@ -6,8 +6,8 @@ this.uiapp={};
 
 
 
-require(["js/pipelines/pipeline_type.js", "js/pipelines/helper_display.js"],
-        function(Pipeline, display) {
+require(["js/pipelines/pipeline_type.js", "js/pipelines/helper_display.js","js/async.js"],
+        function(Pipeline, display, async) {
 
             var transformation_chainable1_fn=function(data_state, callback){
                 setTimeout(function () {
@@ -29,13 +29,11 @@ require(["js/pipelines/pipeline_type.js", "js/pipelines/helper_display.js"],
                 }, 250);
             };
 
-   var transformation_chainable3_fn=function(data_state, callback){
+            var transformation_chainable3_fn=function(data_state, callback){
                 setTimeout(function () {
                     var user_history=[];
                     user_history.push("have dinner");
                     user_history.push("go to bed");
-                    
-
                     data_state.user_history.push.apply(data_state.user_history, user_history);
                     callback(null, data_state);
                 }, 250);
@@ -43,37 +41,77 @@ require(["js/pipelines/pipeline_type.js", "js/pipelines/helper_display.js"],
             
 
             var on_success=function(res, pipeline){
-               
+                
                 pipeline.getSteps().map(
                     function(step){
-                         display.jqueryIterateAndDisplayHistoryStep("#left", step.ns, step,  "user_history");
+                        display.jqueryIterateAndDisplayHistoryStep("#left", step.ns, step,  "user_history");
                     }
                 );
                 display.jqueryIterateAndDisplayHistoryStep("#center", pipeline.ns, pipeline, "user_history");
 
             };
 
-            var on_error=function(err){alert(err);};
+            var on_error=function(err, pipeline){alert(err); };
 
             var initial_state={user_history:["wake up!"]};
-            function start1(){
-            new Pipeline("pipeline1")
+
+            function getPipeline1(){
+                return             new Pipeline("pipeline1")
                     .addTransformation("Good_Morning", transformation_chainable1_fn)
-                    .addTransformation("Good_Afternoon", transformation_chainable2_fn)
-                    .transform(initial_state, 
-                               function(res, pipeline){
-                                   on_success(res, pipeline); 
-                                   initial_state=res;
-                                   $('#start_pipeline').prop("value", "next pipeline!").off('click').click(start2);}, on_error);
-        }
+                    .addTransformation("Good_Afternoon", transformation_chainable2_fn);
+
+            }
+
+            function getPipeline2(){
+                return  new Pipeline("pipeline2")
+                    .addTransformation("Good_Night", transformation_chainable3_fn);
+
+            }
+            function start1(){
+                var  on_success_start1=function(res, pipeline){
+                    on_success(res, pipeline); 
+                    initial_state=res;
+                    $('#start_pipeline').prop("value", "next pipeline!").off('click').click(start2);};
+                getPipeline1()
+                    .set_on_success(on_success_start1)
+                    .set_on_error(on_error)
+                    .apply_transformations(initial_state);
+            }
 
             function start2(){
-            new Pipeline("pipeline2")
-                    .addTransformation("Good_Night", transformation_chainable3_fn)
-
-                    .transform(initial_state, on_success, on_error);
+                
+                getPipeline2().set_on_success(on_success).set_on_error(on_error)
+                    .apply_transformations(initial_state);
             }
+            function get_alert(message){
+                return function(res, pipeline){alert(message+"pipeline: "+ ((pipeline)? pipeline.ns: "no_pipeline")+"\n"+toJson((res)? res : "no res!"));};
+            }
+
+            function compose_it(){
+                var  on_success_pipe=function(message){
+                    return function(res, pipeline){
+                        on_success(res, pipeline); 
+                        get_alert(message);
+                    };};
+
+
+
+
+
+                var pipe_1=getPipeline1().set_on_success(on_success_pipe("success11111")).set_on_error("error 1");
+                var pipe_2=getPipeline2().set_on_success(on_success_pipe("success222")).set_on_error("error 2");
+                var compose=  new Pipeline("pipeline_compose!").set_on_success(get_alert("success::: composing")).set_on_error(get_alert("error on composing"));
+                compose.addPipe(pipe_1).addPipe(pipe_2);
+                compose.apply_transformations({user_history:["composing history!!"]});
+                
+
+            }
+
+
             $('#start_pipeline').click(start1);
 
 
-});
+            $('#compose_pipelines').click(function(){compose_it();});
+
+
+        });
