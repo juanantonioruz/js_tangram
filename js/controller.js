8efbbadcac87c1d883772c0b10d160073c144193
juanantonioruz <juanantonioruz@gmail.com>
2013-08-27 13:13:36 +0200

    fixed bug jquery.min.map not found with map js file

--- js/controller.js
+++ js/controller.js
@@ -7,13 +7,13 @@ require(["js/start_ew.js", "js/start_proposal.js", "js/start_dev.js"],
             function getURLParameter(name) {
                 return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
             }
-            if(getURLParameter("dev"))
-                dev(); 
-            else if (getURLParameter("proposal"))
-                proposal();
-            // else if (getURLParameter("ew"))
-            //     ew();
-            else
+            // if(getURLParameter("dev"))
+            //     dev(); 
+            // else if (getURLParameter("proposal"))
+            //     proposal();
+            // // else if (getURLParameter("ew"))
+            // //     ew();
+            // else
                 proposal();
 
 
